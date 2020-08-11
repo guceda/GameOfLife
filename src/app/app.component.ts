8d56c6e8a237b40d94d50ad4221c9813b87477d4
Gonzalo Uceda Castro <gonzaloucedacastro@Gonzalos-MacBook-Pro.local>
2020-08-11 20:51:39 +0200

    add stats

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { Board } from './models/board.model';
 
+import * as Highcharts from 'highcharts/highcharts';
+import Exporting from 'highcharts/modules/exporting';
+Exporting(Highcharts);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,28 +16,54 @@ export class AppComponent {
   dragStatus: number;
   generation: number;
   gameStatus: number; // -1 iddle; 0 active; 1 Paused
-  initTime: number;
   interval: number;
+
+  stats: number[];
+  chart: any;
 
   board: Board;
 
   constructor() {
-    this.numCols = 50;
-    this.numRows = 50;
+    this.numCols = 40;
+    this.numRows = 40;
     this.generation = 0;
     this.gameStatus = -1;
     this.dragStatus = 0;
     this.interval = null;
-    this.initTime = null;
+
+    this.chart = null;
+    this.stats = [];
 
     this.board = new Board(this.numCols, this.numRows);
   }
+
+  ngOnInit() {
+    this.chart = Highcharts.chart('chart', {
+      tooltip: { enabled: false },
+      title: false,
+      subtitle: false,
+      exporting: { enabled: false },
+      chart: { type: 'line' },
+      legend: { enabled: false },
+      xAxis: { visible:false },
+      yAxis: { type:'logarithmic', visible:false},
+      credits: { enabled: false },
+      series: [{ data: [], color:'coral' }],
+      plotOptions: { series: { marker: { enabled: false } } }
+    });
+  }
+
 
   start() {
     this.interval = setInterval(() => {
       if (this.gameStatus === 0) {
         this.board.checkBoard();
         this.generation++;
+        if (this.stats.length >= 1000) {
+          this.stats.shift();
+        }
+        this.stats.push(this.board.alive);
+        this.chart.series[0].setData(this.stats);
       }
     }, 100);
   }
@@ -57,9 +87,17 @@ export class AppComponent {
   }
 
   onClear() {
+    this.chart.series[0].setData([]);
     this.gameStatus = -1;
     this.board.reset();
     this.generation = 0;
     clearInterval(this.interval);
+  }
+
+  randomize() {
+    this.onClear();
+    this.board.randomize();
+    this.gameStatus = this.gameStatus === 0 ? 1 : 0;
+    this.start();
   }
 }
